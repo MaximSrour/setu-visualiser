25369a6e11a340aefb03acbf17891e34d16a4ef3
Maxim Srour <maxim.srour@gmail.com>
2024-01-22 03:51:02 +1100

    Fixed bugs

--- src/components/ui/github-button.tsx
+++ src/components/ui/github-button.tsx
@@ -3,39 +3,48 @@
 import * as React from "react"
 import Link from "next/link";
 
-import { GitHubLogoIcon } from "@radix-ui/react-icons"
+import { GitHubLogoIcon, StarFilledIcon } from "@radix-ui/react-icons"
 
 import { Button } from "~/components/ui/button"
 
+interface IGitHubResponse {
+	message?: string;
+	stargazers_count?: number;
+}
+
 interface IGithubButtonProps {
 	repo: string;
+	showStars?: boolean;
 }
 
 export default function GithubButton(props: IGithubButtonProps) {
-	const [repoMessage, setRepoMessage] = React.useState<string>(null!);
+	const [repoResponse, setRepoResponse] = React.useState<IGitHubResponse>(null!);
+	
 	React.useEffect(() => {
-		interface IGitHubResponse {
-			message?: string;
-		}
-
 		async function fetchData() {
 			await fetch(`https://api.github.com/repos/${props.repo}`)
 			.then(res => res.json() as Promise<IGitHubResponse>)
-			.then(data => setRepoMessage(data?.message ?? "Not Found"))
-			.catch(() => setRepoMessage("Not Found"));
+			.then(data => setRepoResponse(data))
+			.catch(() => setRepoResponse({ message: "Not Found" }));
 		}
 
 		void fetchData();
 	}, [props.repo]);
 
-	if(!repoMessage || repoMessage == "Not Found") {
+	if(!repoResponse || repoResponse.message == "Not Found") {
 		return null;
 	}
 
 	return (
 		<Link href={`https://github.com/${props.repo}`} target="_blank">
-			<Button variant="outline" size="icon">
+			<Button variant="outline" size="icon" className="flex flex-row gap-2 w-auto p-3">
 				<GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />
+				
+				{props.showStars && <span className="flex flex-row gap-1 items-center justify-center">
+					{repoResponse.stargazers_count}
+					<StarFilledIcon className="h-[0.8rem] w-[0.8rem]" />
+				</span>}
+				
 				<span className="sr-only">Github</span>
 			</Button>
 		</Link>

f5967ccbc2d95a2a68eb7332382f47d2c68a510c
Maxim Srour <maxim.srour@gmail.com>
2024-01-23 01:24:17 +1100

    Fixed invalid prettier config, formatted all

--- src/components/CustomTooltip.tsx
+++ src/components/CustomTooltip.tsx
@@ -1,45 +1,56 @@
-import React from 'react';
-import type { TooltipProps } from 'recharts';
+import React from "react";
+import type { TooltipProps } from "recharts";
 
 export interface IRefArea {
-	min: number;
-	max: number;
-	color: string;
-	description: string;
+  min: number;
+  max: number;
+  color: string;
+  description: string;
 }
 
 export const refAreas: IRefArea[] = [
-	{ min: 0, max: 3, color: "red", description: "Needing critical attention" },
-	{ min: 3, max: 3.8, color: "yellow", description: "Needing improvement" },
-	{ min: 3.8, max: 4.7, color: "green", description: "Meeting requirements" },
-	{ min: 4.7, max: 6, color: "purple", description: "Exceeding requirements" },
+  { min: 0, max: 3, color: "red", description: "Needing critical attention" },
+  { min: 3, max: 3.8, color: "yellow", description: "Needing improvement" },
+  { min: 3.8, max: 4.7, color: "green", description: "Meeting requirements" },
+  { min: 4.7, max: 6, color: "purple", description: "Exceeding requirements" },
 ];
 
 export function getRefArea(value: number): IRefArea | null {
-	for (const refArea of refAreas) {
-		if (refArea.min <= value && refArea.max > value) {
-			return refArea;
-		}
-	}
+  for (const refArea of refAreas) {
+    if (refArea.min <= value && refArea.max > value) {
+      return refArea;
+    }
+  }
 
-	return null;
+  return null;
 }
 
-export default function CustomTooltip({active, payload}: TooltipProps<number, string>) {
-	if (active && payload?.length) {
-		const value = payload[0]?.value;
-		const refArea = getRefArea(value ?? 0);
+export default function CustomTooltip({
+  active,
+  payload,
+}: TooltipProps<number, string>) {
+  if (active && payload?.length) {
+    const value = payload[0]?.value;
+    const refArea = getRefArea(value ?? 0);
 
-		return (
-			<div className="bg-slate-800 rounded-md p-3">
-				<p className="text-white">{`${value}`}</p>
-				{ refArea?.color == "red" && <p className="text-red-700">{refArea?.description}</p>}
-				{ refArea?.color == "yellow" && <p className="text-amber-400">{refArea?.description}</p>}
-				{ refArea?.color == "green" && <p className="text-green-500">{refArea?.description}</p>}
-				{ refArea?.color == "purple" && <p className="text-indigo-700">{refArea?.description}</p>}
-			</div>
-		);
-	}
+    return (
+      <div className="rounded-md bg-slate-800 p-3">
+        <p className="text-white">{`${value}`}</p>
+        {refArea?.color == "red" && (
+          <p className="text-red-700">{refArea?.description}</p>
+        )}
+        {refArea?.color == "yellow" && (
+          <p className="text-amber-400">{refArea?.description}</p>
+        )}
+        {refArea?.color == "green" && (
+          <p className="text-green-500">{refArea?.description}</p>
+        )}
+        {refArea?.color == "purple" && (
+          <p className="text-indigo-700">{refArea?.description}</p>
+        )}
+      </div>
+    );
+  }
 
-	return null;
-};
+  return null;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,26 @@ import Head from "next/head";
 import Header from "~/components/Header";
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>SETU Visualiser</title>
-				<meta name="description" content="Visualiser for Monash SETU data" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			
-			<Header />
+  return (
+    <>
+      <Head>
+        <title>SETU Visualiser</title>
+        <meta name="description" content="Visualiser for Monash SETU data" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			<main className="flex min-h-screen flex-col items-center justify-center">
-				<div className="flex flex-col items-center justify-center gap-12 px-4 py-16 w-full">
-					<span className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-						SETU<span className="text-[hsl(280,100%,70%)]">.</span>fyi
-					</span>
-					<p className="text-xl text-center">The comprehensive visualiser for unit performance</p>
-				</div>
-			</main>
-		</>
-	);
+      <Header />
+
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div className="flex w-full flex-col items-center justify-center gap-12 px-4 py-16">
+          <span className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            SETU<span className="text-[hsl(280,100%,70%)]">.</span>fyi
+          </span>
+          <p className="text-center text-xl">
+            The comprehensive visualiser for unit performance
+          </p>
+        </div>
+      </main>
+    </>
+  );
 }

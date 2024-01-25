30ce8630e01f8684eba70747e6ee23ee8c66ced2
Maxim Srour <maxim.srour@gmail.com>
2024-01-25 20:18:47 +1100

    Actually fixed refArea bug

--- src/pages/unit.tsx
+++ src/pages/unit.tsx
@@ -89,7 +89,7 @@ export default function Home() {
 
 							void fetchData();
 						}}
-						value={search}
+						defaultValue={search}
 					/>
 
 					<div className="grid max-w-5xl grid-cols-1 gap-2 sm:grid-cols-2 md:gap-8 lg:grid-cols-3">

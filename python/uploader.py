174fc40f0e84b9484e0e0d2d55b28613e104a4fa
Maxim Srour <maxim.srour@gmail.com>
2024-03-11 22:24:55 +1100

    misc: Added some testing code

--- python/uploader.py
+++ python/uploader.py
@@ -3,6 +3,7 @@ load_dotenv()
 import os
 import MySQLdb
 import csv
+from tqdm import tqdm
 
 class dbTable:
 	def __init__(self, targetTable, source):
@@ -14,8 +15,6 @@ tables = {
 	"aspectData": dbTable("setu-visualiser_aspect_data", "aspectData.csv"),
 	"aspectDefinitions": dbTable("setu-visualiser_aspect_definitions", "aspectDefs.csv"),
 }
-
-TABLE = tables["aspectData"]
 
 # Establish connection
 connection = MySQLdb.connect(
@@ -30,28 +29,35 @@ connection = MySQLdb.connect(
 	}
 )
 
-with open(TABLE.source, 'r') as file:
-	csv_data = csv.reader(file)
-	
-	# Extract column names from the first row (header)
-	columns = next(csv_data)
-	placeholders = ', '.join(['%s'] * len(columns))
-	columns_formatted = ', '.join(columns)
-	
-	# Prepare SQL statement
-	sql = f"INSERT INTO `{TABLE.targetTable}` ({columns_formatted}) VALUES ({placeholders})"
+for table in tables.values():
+	print(f"Uploading {table.source} to {table.targetTable}...")
 
-	# Convert the rest of the CSV lines into a list of tuples
-	data_tuples = list(map(tuple, csv_data))
+	with open(table.source, 'r') as file:
+		csv_data = csv.reader(file)
+		
+		# Extract column names from the first row (header)
+		columns = next(csv_data)
+		placeholders = ', '.join(['%s'] * len(columns))
+		columns_formatted = ', '.join(columns)
+		
+		# Prepare SQL statement
+		sql = f"INSERT INTO `{table.targetTable}` ({columns_formatted}) VALUES ({placeholders})"
 
-# Cursor to execute queries
-cursor = connection.cursor()
+		# Convert the rest of the CSV lines into a list of tuples
+		data_tuples = list(map(tuple, csv_data))
 
-for data_tuple in data_tuples:
-	try:
-		cursor.execute(sql, data_tuple)
-	except Exception as e:
-		pass
+	# Cursor to execute queries
+	cursor = connection.cursor()
+
+	pbar = tqdm(data_tuples)
+	for data_tuple in tqdm(data_tuples):
+		pbar.set_description("_".join(data_tuple[:5]))
+
+		try:
+			cursor.execute(sql, data_tuple)
+		except Exception as e:
+			pass
+
 # Close cursor and connection
 cursor.close()
 connection.close()
